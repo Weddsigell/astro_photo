b99b3830f2d9bfb4a890466f8f4d6b7f0796caf8
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-02-02 14:41:24 +0500

    publish photo to Telegram channel

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     tg_tоken = env.str('TG_TOKEN')
     bot = telegram.Bot(tg_tоken)
     bot.send_message(text='Добро пожаловать в канал с астро фото', chat_id=env.str('TG_ID_CHANEL'))
+    bot.send_document(chat_id=env.str('TG_ID_CHANEL'), document=open('./images/epic_0.png', 'rb'))
 
 
 if __name__ == '__main__':
